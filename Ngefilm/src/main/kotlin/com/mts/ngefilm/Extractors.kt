0fef088e6302df9eefbabe65d1beb0242f3772d7
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-06-27 15:21:44 +0800

    chore: update [main] [skip ci]

---
+++ Ngefilm/src/main/kotlin/com/mts/ngefilm/Extractors.kt
@@ -0,0 +1,193 @@
+package com.mts.ngefilm
+
+import com.lagradost.cloudstream3.extractors.StreamWishExtractor
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.ExtractorLinkType
+import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.newExtractorLink
+
+class Ratu89News : StreamWishExtractor() {
+    override val name = "Ratu89News"
+    override val mainUrl = "https://ratu89.news"
+}
+
+class ApkblockS3Apnortheast1AmazonawsCom : StreamWishExtractor() {
+    override val name = "ApkblockS3Apnortheast1AmazonawsCom"
+    override val mainUrl = "https://apk-block.s3.ap-northeast-1.amazonaws.com"
+}
+
+class Gratu89Com : StreamWishExtractor() {
+    override val name = "Gratu89Com"
+    override val mainUrl = "https://gratu89.com"
+}
+
+class Judi89News : StreamWishExtractor() {
+    override val name = "Judi89News"
+    override val mainUrl = "https://judi89.news"
+}
+
+class FkuponCom : StreamWishExtractor() {
+    override val name = "FkuponCom"
+    override val mainUrl = "https://fkupon.com"
+}
+
+class BioSite : StreamWishExtractor() {
+    override val name = "BioSite"
+    override val mainUrl = "https://bio.site"
+}
+
+class HalalhomecookingCom : StreamWishExtractor() {
+    override val name = "HalalhomecookingCom"
+    override val mainUrl = "https://halalhomecooking.com"
+}
+
+class WapBm88Net : StreamWishExtractor() {
+    override val name = "WapBm88Net"
+    override val mainUrl = "https://wap.bm-88.net"
+}
+
+class HistoryJlfafafa3Com : StreamWishExtractor() {
+    override val name = "HistoryJlfafafa3Com"
+    override val mainUrl = "https://history.jlfafafa3.com"
+}
+
+class CdnAmpprojectOrg : StreamWishExtractor() {
+    override val name = "CdnAmpprojectOrg"
+    override val mainUrl = "https://cdn.ampproject.org"
+}
+
+class CampaignPastiwddarikdslotCom : StreamWishExtractor() {
+    override val name = "CampaignPastiwddarikdslotCom"
+    override val mainUrl = "https://campaign.pastiwddarikdslot.com"
+}
+
+class KdslotmainCom : StreamWishExtractor() {
+    override val name = "KdslotmainCom"
+    override val mainUrl = "https://kdslotmain.com"
+}
+
+class KdslotidnCom : StreamWishExtractor() {
+    override val name = "KdslotidnCom"
+    override val mainUrl = "https://kdslotidn.com"
+}
+
+class KdpokersCom : StreamWishExtractor() {
+    override val name = "KdpokersCom"
+    override val mainUrl = "https://kdpokers.com"
+}
+
+class ApkbersamakdCom : StreamWishExtractor() {
+    override val name = "ApkbersamakdCom"
+    override val mainUrl = "https://apkbersamakd.com"
+}
+
+class TautinApp : StreamWishExtractor() {
+    override val name = "TautinApp"
+    override val mainUrl = "https://tautin.app"
+}
+
+class CdnLivechatincCom : StreamWishExtractor() {
+    override val name = "CdnLivechatincCom"
+    override val mainUrl = "https://cdn.livechatinc.com"
+}
+
+class Jseu1HsscriptsCom : StreamWishExtractor() {
+    override val name = "Jseu1HsscriptsCom"
+    override val mainUrl = "https://js-eu1.hs-scripts.com"
+}
+
+class MyLivechatincCom : StreamWishExtractor() {
+    override val name = "MyLivechatincCom"
+    override val mainUrl = "https://my.livechatinc.com"
+}
+
+class AccountsLivechatCom : StreamWishExtractor() {
+    override val name = "AccountsLivechatCom"
+    override val mainUrl = "https://accounts.livechat.com"
+}
+
+class TextCom : StreamWishExtractor() {
+    override val name = "TextCom"
+    override val mainUrl = "https://text.com"
+}
+
+class PlatformTextCom : StreamWishExtractor() {
+    override val name = "PlatformTextCom"
+    override val mainUrl = "https://platform.text.com"
+}
+
+class LineMe : StreamWishExtractor() {
+    override val name = "LineMe"
+    override val mainUrl = "https://line.me"
+}
+
+class CMicrosoftCom : StreamWishExtractor() {
+    override val name = "CMicrosoftCom"
+    override val mainUrl = "https://c.microsoft.com"
+}
+
+class StaticsTeamsCdnLiveNet : StreamWishExtractor() {
+    override val name = "StaticsTeamsCdnLiveNet"
+    override val mainUrl = "https://statics.teams.cdn.live.net"
+}
+
+class ApiLivechatincCom : StreamWishExtractor() {
+    override val name = "ApiLivechatincCom"
+    override val mainUrl = "https://api.livechatinc.com"
+}
+
+class KdslotproSpace : StreamWishExtractor() {
+    override val name = "KdslotproSpace"
+    override val mainUrl = "https://kdslotpro.space"
+}
+
+class DewajudiInk : StreamWishExtractor() {
+    override val name = "DewajudiInk"
+    override val mainUrl = "https://dewajudi.ink"
+}
+
+class ApkdepotS3Apnortheast1AmazonawsCom : StreamWishExtractor() {
+    override val name = "ApkdepotS3Apnortheast1AmazonawsCom"
+    override val mainUrl = "https://apk-depot.s3.ap-northeast-1.amazonaws.com"
+}
+
+class HgcloudTo : StreamWishExtractor() {
+    override val name = "HgcloudTo"
+    override val mainUrl = "https://hgcloud.to"
+}
+
+class MorenciusCom : StreamWishExtractor() {
+    override val name = "MorenciusCom"
+    override val mainUrl = "https://morencius.com"
+}
+
+class IYtimgCom : StreamWishExtractor() {
+    override val name = "IYtimgCom"
+    override val mainUrl = "https://i.ytimg.com"
+}
+
+class YoutuBe : StreamWishExtractor() {
+    override val name = "YoutuBe"
+    override val mainUrl = "https://youtu.be"
+}
+
+class KdslotaksesKdslotidnShop : StreamWishExtractor() {
+    override val name = "KdslotaksesKdslotidnShop"
+    override val mainUrl = "https://kdslotakses.kdslotidn.shop"
+}
+
+class KdslolacsOnline : StreamWishExtractor() {
+    override val name = "KdslolacsOnline"
+    override val mainUrl = "https://kdslolacs.online"
+}
+
+class LivechatCom : StreamWishExtractor() {
+    override val name = "LivechatCom"
+    override val mainUrl = "https://livechat.com"
+}
+
+class TeamsLiveCom : StreamWishExtractor() {
+    override val name = "TeamsLiveCom"
+    override val mainUrl = "https://teams.live.com"
+}
