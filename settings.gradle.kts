38ef716f3f18372fcb4cbc44f7029cbef7e44243
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-06-27 13:34:49 +0800

    chore: update [main] [skip ci]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,3 @@
 rootProject.name = "cloudstream-extensions"
 
-include(":Pencurimovie")
-include(":Anoboy")
-include(":Cinemax21")
-include(":Oploverz")
-include(":Donghub")
-include(":Dutamovie21")
 include(":Anichin")
-include(":Layarotaku")
