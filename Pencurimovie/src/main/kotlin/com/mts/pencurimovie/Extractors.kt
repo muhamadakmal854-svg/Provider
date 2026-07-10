583b8667e9ef4f8093bc28cb7dd191b81f3fee82
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-07-10 16:27:35 +0800

    chore: update [main] [skip ci]

--- Pencurimovie/src/main/kotlin/com/mts/pencurimovie/Extractors.kt
+++ Pencurimovie/src/main/kotlin/com/mts/pencurimovie/Extractors.kt
@@ -1,27 +1,338 @@
 package com.mts.pencurimovie
 
+import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.DoodLaExtractor
+import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.StreamWishExtractor
+import com.lagradost.cloudstream3.extractors.Voe
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.USER_AGENT
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
-import com.lagradost.cloudstream3.base64Decode
-import com.lagradost.cloudstream3.extractors.StreamWishExtractor
-import com.lagradost.cloudstream3.extractors.DoodLaExtractor
-import com.fasterxml.jackson.annotation.JsonProperty
-import java.net.URI
+import com.lagradost.cloudstream3.utils.loadExtractor
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import java.security.MessageDigest
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
-
-class Hglink : StreamWishExtractor() {
-    override val name = "Hglink"
-    override val mainUrl = "https://hglink.to"
+class PushsdkNet : StreamWishExtractor() {
+    override var name = "PushsdkNet"
+    override var mainUrl = "https://push-sdk.net"
 }
 
-class Dsvplay : DoodLaExtractor() {
+class ApiSubsourceNet : StreamWishExtractor() {
+    override var name = "ApiSubsourceNet"
+    override var mainUrl = "https://api.subsource.net"
+}
+
+class CdnSubsourceNet : StreamWishExtractor() {
+    override var name = "CdnSubsourceNet"
+    override var mainUrl = "https://cdn.subsource.net"
+}
+
+class InsignificantconstantCom : StreamWishExtractor() {
+    override var name = "InsignificantconstantCom"
+    override var mainUrl = "https://insignificant-constant.com"
+}
+
+class IDoodcdnIo : DoodLaExtractor() {
+    override var mainUrl = "https://i.doodcdn.io"
+}
+
+class DoimgNet : StreamWishExtractor() {
+    override var name = "DoimgNet"
+    override var mainUrl = "https://doimg.net"
+}
+
+class HelpDoodstreamCom : DoodLaExtractor() {
+    override var mainUrl = "https://help.doodstream.com"
+}
+
+class Wws306LCloudatacdnCom : StreamWishExtractor() {
+    override var name = "Wws306LCloudatacdnCom"
+    override var mainUrl = "https://wws306l.cloudatacdn.com"
+}
+
+class Static2DoodcdnIo : DoodLaExtractor() {
+    override var mainUrl = "https://static2.doodcdn.io"
+}
+
+class HgcloudTo : StreamWishExtractor() {
+    override var name = "HgcloudTo"
+    override var mainUrl = "https://hgcloud.to"
+}
+
+class VoeSx : Voe() {
+    override var name = "VoeSx"
+    override var mainUrl = "https://voe.sx"
+}
+
+class StreamtapeCom : StreamTape() {
+    override var name = "StreamtapeCom"
+    override var mainUrl = "https://streamtape.com"
+}
+
+class MinochinosCom : StreamWishExtractor() {
+    override var name = "MinochinosCom"
+    override var mainUrl = "https://minochinos.com"
+}
+
+class SubsourceNet : StreamWishExtractor() {
+    override var name = "SubsourceNet"
+    override var mainUrl = "https://subsource.net"
+}
+
+class AnchurlCom : StreamWishExtractor() {
+    override var name = "AnchurlCom"
+    override var mainUrl = "https://anchurl.com"
+}
+
+class CertakerInfo : StreamWishExtractor() {
+    override var name = "CertakerInfo"
+    override var mainUrl = "https://certaker.info"
+}
+
+class UploadWikimediaOrg : StreamWishExtractor() {
+    override var name = "UploadWikimediaOrg"
+    override var mainUrl = "https://upload.wikimedia.org"
+}
+
+class Undefined : StreamWishExtractor() {
+    override var name = "Undefined"
+    override var mainUrl = "https://undefined"
+}
+
+class OsLiplesssaligotCom : StreamWishExtractor() {
+    override var name = "OsLiplesssaligotCom"
+    override var mainUrl = "https://os.liplesssaligot.com"
+}
+
+class HglinkTo : StreamWishExtractor() {
+    override var name = "HglinkTo"
+    override var mainUrl = "https://hglink.to"
+}
+
+class ListeamedNet : StreamWishExtractor() {
+    override var name = "ListeamedNet"
+    override var mainUrl = "https://listeamed.net"
+}
+
+class BigwarpPro : StreamWishExtractor() {
+    override var name = "BigwarpPro"
+    override var mainUrl = "https://bigwarp.pro"
+}
+
+class Dd315OCloudatacdnCom : StreamWishExtractor() {
+    override var name = "Dd315OCloudatacdnCom"
+    override var mainUrl = "https://dd315o.cloudatacdn.com"
+}
+
+class AbyssplayerCom : ExtractorApi() {
+    override var name = "AbyssplayerCom"
+    override var mainUrl = "https://abyssplayer.com"
+    override val requiresReferer = true
+
+    private fun decryptAesCtr(ciphertext: ByteArray, key: ByteArray, iv: ByteArray): ByteArray {
+        val spec = javax.crypto.spec.SecretKeySpec(key, "AES")
+        val parameterSpec = javax.crypto.spec.IvParameterSpec(iv)
+        val cipher = javax.crypto.Cipher.getInstance("AES/CTR/NoPadding")
+        cipher.init(javax.crypto.Cipher.DECRYPT_MODE, spec, parameterSpec)
+        return cipher.doFinal(ciphertext)
+    }
+
+    private fun md5(input: ByteArray): ByteArray {
+        val md = java.security.MessageDigest.getInstance("MD5")
+        return md.digest(input)
+    }
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        try {
+            val cleanUrl = url.replace(92.toChar().toString(), "")
+            val pageHtml = app.get(cleanUrl, headers = mapOf("Referer" to (referer ?: mainUrl))).text
+
+            val rx = Regex("const datas\\s*=\\s*\"([^\"]+)\"")
+            val base64Str = rx.find(pageHtml)?.groupValues?.get(1) ?: return
+            val decodedBytes = android.util.Base64.decode(base64Str, android.util.Base64.DEFAULT)
+            val latin1Str = String(decodedBytes, Charsets.ISO_8859_1)
+
+            val json = org.json.JSONObject(latin1Str)
+            val slug = json.getString("slug")
+            val userId = json.getString("user_id")
+            val md5Id = json.getString("md5_id")
+            val media = json.getString("media")
+
+            val keyStr = "$userId:$slug:$md5Id"
+            val keyBytesStr = md5(keyStr.toByteArray(Charsets.UTF_8)).joinToString("") { "%02x".format(it) }
+            val key = keyBytesStr.toByteArray(Charsets.UTF_8)
+            val iv = key.sliceArray(0 until 16)
+
+            val mediaCiphertext = android.util.Base64.decode(media, android.util.Base64.DEFAULT)
+            val decryptedMediaBytes = decryptAesCtr(mediaCiphertext, key, iv)
+            val decryptedMediaStr = String(decryptedMediaBytes, Charsets.UTF_8)
+
+            val mediaJson = org.json.JSONObject(decryptedMediaStr)
+            val mp4 = mediaJson.getJSONObject("mp4")
+            val sources = mp4.getJSONArray("sources")
+            val domainsObj = if (mp4.has("domains")) mp4.getJSONObject("domains") else if (mediaJson.has("domains")) mediaJson.getJSONObject("domains") else org.json.JSONObject()
+
+            for (i in 0 until sources.length()) {
+                val src = sources.getJSONObject(i)
+                val size = src.getLong("size")
+                val resId = src.getInt("res_id")
+                val label = src.getString("label")
+                val sub = src.getString("sub")
+
+                val domain = domainsObj.getString(sub)
+
+                val pathStr = "/mp4/$md5Id/$resId/$size?v=$slug"
+                val sizeStr = size.toString()
+                val digitBytes = sizeStr.map { it.toString().toInt().toByte() }.toByteArray()
+                val sizeHashHex = md5(digitBytes).joinToString("") { "%02x".format(it) }
+                val pathKey = sizeHashHex.toByteArray(Charsets.UTF_8)
+                val pathIv = pathKey.sliceArray(0 until 16)
+
+                val pathBytes = pathStr.toByteArray(Charsets.UTF_8)
+                val encryptedPathBytes = decryptAesCtr(pathBytes, pathKey, pathIv)
+
+                val b64Once = android.util.Base64.encodeToString(encryptedPathBytes, android.util.Base64.NO_WRAP)
+                val b64Twice = android.util.Base64.encodeToString(b64Once.toByteArray(Charsets.UTF_8), android.util.Base64.NO_WRAP)
+                val cleanPath = b64Twice.replace("=", "").replace("\n", "").replace("\r", "")
+
+                val finalStreamUrl = "https://$domain/sora/$size/$cleanPath"
+
+                callback(
+                    newExtractorLink(
+                        source = name,
+                        name = "$name - $label",
+                        url = finalStreamUrl,
+                        type = ExtractorLinkType.VIDEO
+                    ) {
+                        this.referer = cleanUrl
+                        this.quality = when (label.lowercase()) {
+                            "360p" -> Qualities.P360.value
+                            "480p" -> Qualities.P480.value
+                            "720p" -> Qualities.P720.value
+                            "1080p" -> Qualities.P1080.value
+                            else -> Qualities.Unknown.value
+                        }
+                    }
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+class AbysscdnCom : ExtractorApi() {
+    override var name = "AbysscdnCom"
+    override var mainUrl = "https://abysscdn.com"
+    override val requiresReferer = true
+
+    private fun decryptAesCtr(ciphertext: ByteArray, key: ByteArray, iv: ByteArray): ByteArray {
+        val spec = javax.crypto.spec.SecretKeySpec(key, "AES")
+        val parameterSpec = javax.crypto.spec.IvParameterSpec(iv)
+        val cipher = javax.crypto.Cipher.getInstance("AES/CTR/NoPadding")
+        cipher.init(javax.crypto.Cipher.DECRYPT_MODE, spec, parameterSpec)
+        return cipher.doFinal(ciphertext)
+    }
+
+    private fun md5(input: ByteArray): ByteArray {
+        val md = java.security.MessageDigest.getInstance("MD5")
+        return md.digest(input)
+    }
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        try {
+            val cleanUrl = url.replace(92.toChar().toString(), "")
+            val pageHtml = app.get(cleanUrl, headers = mapOf("Referer" to (referer ?: mainUrl))).text
+
+            val rx = Regex("const datas\\s*=\\s*\"([^\"]+)\"")
+            val base64Str = rx.find(pageHtml)?.groupValues?.get(1) ?: return
+            val decodedBytes = android.util.Base64.decode(base64Str, android.util.Base64.DEFAULT)
+            val latin1Str = String(decodedBytes, Charsets.ISO_8859_1)
+
+            val json = org.json.JSONObject(latin1Str)
+            val slug = json.getString("slug")
+            val userId = json.getString("user_id")
+            val md5Id = json.getString("md5_id")
+            val media = json.getString("media")
+
+            val keyStr = "$userId:$slug:$md5Id"
+            val keyBytesStr = md5(keyStr.toByteArray(Charsets.UTF_8)).joinToString("") { "%02x".format(it) }
+            val key = keyBytesStr.toByteArray(Charsets.UTF_8)
+            val iv = key.sliceArray(0 until 16)
+
+            val mediaCiphertext = android.util.Base64.decode(media, android.util.Base64.DEFAULT)
+            val decryptedMediaBytes = decryptAesCtr(mediaCiphertext, key, iv)
+            val decryptedMediaStr = String(decryptedMediaBytes, Charsets.UTF_8)
+
+            val mediaJson = org.json.JSONObject(decryptedMediaStr)
+            val mp4 = mediaJson.getJSONObject("mp4")
+            val sources = mp4.getJSONArray("sources")
+            val domainsObj = if (mp4.has("domains")) mp4.getJSONObject("domains") else if (mediaJson.has("domains")) mediaJson.getJSONObject("domains") else org.json.JSONObject()
+
+            for (i in 0 until sources.length()) {
+                val src = sources.getJSONObject(i)
+                val size = src.getLong("size")
+                val resId = src.getInt("res_id")
+                val label = src.getString("label")
+                val sub = src.getString("sub")
+
+                val domain = domainsObj.getString(sub)
+
+                val pathStr = "/mp4/$md5Id/$resId/$size?v=$slug"
+                val sizeStr = size.toString()
+                val digitBytes = sizeStr.map { it.toString().toInt().toByte() }.toByteArray()
+                val sizeHashHex = md5(digitBytes).joinToString("") { "%02x".format(it) }
+                val pathKey = sizeHashHex.toByteArray(Charsets.UTF_8)
+                val pathIv = pathKey.sliceArray(0 until 16)
+
+                val pathBytes = pathStr.toByteArray(Charsets.UTF_8)
+                val encryptedPathBytes = decryptAesCtr(pathBytes, pathKey, pathIv)
+
+                val b64Once = android.util.Base64.encodeToString(encryptedPathBytes, android.util.Base64.NO_WRAP)
+                val b64Twice = android.util.Base64.encodeToString(b64Once.toByteArray(Charsets.UTF_8), android.util.Base64.NO_WRAP)
+                val cleanPath = b64Twice.replace("=", "").replace("\n", "").replace("\r", "")
+
+                val finalStreamUrl = "https://$domain/sora/$size/$cleanPath"
+
+                callback(
+                    newExtractorLink(
+                        source = name,
+                        name = "$name - $label",
+                        url = finalStreamUrl,
+                        type = ExtractorLinkType.VIDEO
+                    ) {
+                        this.referer = cleanUrl
+                        this.quality = when (label.lowercase()) {
+                            "360p" -> Qualities.P360.value
+                            "480p" -> Qualities.P480.value
+                            "720p" -> Qualities.P720.value
+                            "1080p" -> Qualities.P1080.value
+                            else -> Qualities.Unknown.value
+                        }
+                    }
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
+
+class DsvplayCom : DoodLaExtractor() {
     override var mainUrl = "https://dsvplay.com"
 }
