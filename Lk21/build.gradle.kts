bce6ed4aa0cc00be1fdc1b88b4be4d62c357b257
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-07-17 16:01:14 +0800

    chore: update [main] [skip ci]

---
+++ Lk21/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+
+    language    = "id"
+
+    description = "Provider untuk Lk21"
+
+    authors     = listOf("MTS")
+
+    status      = 1
+
+    tvTypes     = listOf("Movie", "TvSeries")
+
+    iconUrl     = "https://tv11.lk21official.cc/favicon.ico"
+
+}
+
+android {
+
+    namespace = "com.mts.lk21"
+
+}
+
