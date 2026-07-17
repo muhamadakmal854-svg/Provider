bcd27e66237d138b324de4eb0ba8891d073719b6
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-07-17 16:15:17 +0800

    chore: update [main] [skip ci]

---
+++ LayarOtaku/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+
+    language    = "id"
+
+    description = "Provider untuk Layar Otaku"
+
+    authors     = listOf("MTS")
+
+    status      = 1
+
+    tvTypes     = listOf("Movie", "TvSeries")
+
+    iconUrl     = "https://www.xml-acronym-demystifier.org/favicon.ico"
+
+}
+
+android {
+
+    namespace = "com.mts.layarotaku"
+
+}
+
