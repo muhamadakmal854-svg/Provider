6115370890a117462b65f39e4eb67f58f86b7d51
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-07-20 11:33:07 +0800

    feat: restore Klikxxi provider alongside DrakorKita

---
+++ Klikxxi/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+
+    language    = "id"
+
+    description = "KlikXXi – Nonton & Streaming Film terbaru sub indo"
+
+    authors     = listOf("MTS")
+
+    status      = 1
+
+    tvTypes     = listOf("Movie", "TvSeries", "Anime", "AsianDrama")
+
+    iconUrl     = "https://za-ydf.org/favicon.ico"
+
+}
+
+android {
+
+    namespace = "com.klikxxi"
+
+}
+
