0af67567158fe081409baf4411137ef32c991e1a
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-07-25 22:21:59 +0800

    chore: update [main]

---
+++ GudangFilm/src/main/kotlin/com/mts/gudangfilm/GudangFilmPlugin.kt
@@ -0,0 +1,26 @@
+package com.mts.gudangfilm
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class GudangFilmPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(GudangFilmProvider())
+        registerExtractorAPI(CdnAmpprojectOrg())
+        registerExtractorAPI(Poker88PlayMe())
+        registerExtractorAPI(MorenciusCom())
+        registerExtractorAPI(Server3VipBcdnNet())
+        registerExtractorAPI(PremicloudNet())
+        registerExtractorAPI(VidhidehubCom())
+        registerExtractorAPI(VidhideplusCom())
+        registerExtractorAPI(BestxStream())
+        registerExtractorAPI(VidhideproCom())
+        registerExtractorAPI(EmbedpyroxXyz())
+        registerExtractorAPI(AbyssplayerCom())
+        registerExtractorAPI(RpmPlayShare())
+        registerExtractorAPI(Embed4MePlay())
+        registerExtractorAPI(GoogleVideo())
+    }
+}
