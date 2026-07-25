deab470dcaabf8c1966e394c60ea586283b5917f
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-07-25 16:47:18 +0800

    chore: update [main]

---
+++ Rebahin/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+
+    language    = "id"
+
+    description = "Provider untuk REBAHIN"
+
+    authors     = listOf("MTS")
+
+    status      = 1
+
+    tvTypes     = listOf("Movie", "TvSeries")
+
+    iconUrl     = "https://165.227.239.221/favicon.ico"
+
+}
+
+android {
+
+    namespace = "com.mts.rebahin"
+
+}
+
