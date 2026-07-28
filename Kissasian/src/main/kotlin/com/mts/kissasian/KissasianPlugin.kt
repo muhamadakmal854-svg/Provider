a0b99826f2758710f16d2362b9d8d6deac895d21
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-07-28 23:42:15 +0800

    chore: update [main]

--- Kissasian/src/main/kotlin/com/mts/kissasian/KissasianPlugin.kt
+++ Kissasian/src/main/kotlin/com/mts/kissasian/KissasianPlugin.kt
@@ -1,20 +1,12 @@
 package com.mts.kissasian
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-
 import com.lagradost.cloudstream3.plugins.Plugin
 
-import android.content.Context
-
 @CloudstreamPlugin
-
 class KissasianPlugin : Plugin() {
-
     override fun load(context: Context) {
-
-        registerMainAPI(KissasianProvider())
-
+        registerMainAPI(Kissasian())
     }
-
 }
-
