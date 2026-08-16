9aea1d2c04b020effc56615889656743089bfa46
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-08-16 11:14:41 +0800

    MTSFlix Gen 5: Update 272 files [main]

--- Dramaboxdb/build.gradle.kts
+++ Dramaboxdb/build.gradle.kts
@@ -1,13 +1,13 @@
 version = 7
 
 cloudstream {
-    language    = "id"
-    description = "MTSFlix Provider Extension for ChineseAnime"
+    language    = "en"
+    description = "MTSFlix Provider Extension for Dramaboxdb"
     authors     = listOf("MTS")
     status      = 1
     tvTypes     = listOf("Movie", "TvSeries", "Anime")
 }
 
 android {
-    namespace = "com.mtsflix.chineseanime"
+    namespace = "com.mtsflix.dramaboxdb"
 }
--- FreeReels/build.gradle.kts
+++ FreeReels/build.gradle.kts
@@ -2,12 +2,12 @@ version = 7
 
 cloudstream {
     language    = "id"
-    description = "MTSFlix Provider Extension for ChineseAnime"
+    description = "MTSFlix Provider Extension for FreeReels"
     authors     = listOf("MTS")
     status      = 1
     tvTypes     = listOf("Movie", "TvSeries", "Anime")
 }
 
 android {
-    namespace = "com.mtsflix.chineseanime"
+    namespace = "com.mtsflix.freereels"
 }
