b37f851c8b9eda8c365d817c1d929f819dfdbc1d
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-09-24 14:28:14 +0800

    chore: update [main]

---
+++ PopcornFlix/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+
+    language    = "en"
+
+    description = "Provider untuk PopcornFlix - Watch Free Movies & TV Shows Online"
+
+    authors     = listOf("MTS")
+
+    status      = 1
+
+    tvTypes     = listOf("Movie", "TvSeries")
+
+    iconUrl     = "https://popcornflix.com/favicon.ico"
+
+}
+
+android {
+
+    namespace = "com.mts.popcornflix"
+
+}
+
