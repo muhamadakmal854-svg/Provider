b0cb0668142203ee9a613a5555b5da9d452a6a46
muhamadakmal854-svg <muhamadakmal854@gmail.com>
2026-09-25 17:49:01 +0800

    chore: update [main]

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ buildscript {
 
         classpath("com.android.tools.build:gradle:8.13.2")
 
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:81b1d424d2")
 
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
 
